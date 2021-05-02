a07834c2c0a048a778ee183451baed7121603885
Lo Yiu Hung <hung12345@gmail.com>
2021-05-02 17:48:52 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showLeds(`
+    . . . . .
+    . . # # .
+    . . . . #
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
